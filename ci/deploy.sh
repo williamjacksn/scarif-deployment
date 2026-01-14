afa3ff013c2388a3eb24ad901390ccb619f879a4
William Jackson <565174+williamjacksn@users.noreply.github.com>
2026-01-14 16:43:03 -0600

    Try using explicit docker context

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,9 @@
+# set up ssh
+install -D ci/ssh_config ~/.ssh/config
 install --mode=600 -D /dev/null ~/.ssh/id_ed25519
 echo "${SSH_PRIVATE_KEY}" > ~/.ssh/id_ed25519
-cp ci/ssh_config ~/.ssh/config
 ssh-keyscan -H scarif.subtlecoolness.com > ~/.ssh/known_hosts
-docker --host ssh://scarif compose --project-name scarif up --build --detach --remove-orphans
+
+docker context create scarif --docker "host=ssh://scarif"
+docker context use scarif
+docker compose --project-name scarif up --build --detach --remove-orphans

c3a0e1a16920407bee5537f933890c63810ab461
William Jackson <565174+williamjacksn@users.noreply.github.com>
2026-01-14 16:54:25 -0600

    Rearrange some files

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # set up ssh
-install -D ci/ssh_config ~/.ssh/config
+install -D .ssh/config ~/.ssh/config
 install --mode=600 -D /dev/null ~/.ssh/id_ed25519
 echo "${SSH_PRIVATE_KEY}" > ~/.ssh/id_ed25519
 ssh-keyscan -H scarif.subtlecoolness.com > ~/.ssh/known_hosts

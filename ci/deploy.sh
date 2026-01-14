afd02c688245ba8ec46f182cae4e964958b1ba38
William Jackson <565174+williamjacksn@users.noreply.github.com>
2026-01-14 15:56:42 -0600

    Try building a custom caddy image

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,5 +2,4 @@ install --mode=600 -D /dev/null ~/.ssh/id_ed25519
 echo "${SSH_PRIVATE_KEY}" > ~/.ssh/id_ed25519
 cp ci/ssh_config ~/.ssh/config
 ssh-keyscan -H scarif.subtlecoolness.com > ~/.ssh/known_hosts
-scp Caddyfile scarif:/home/rocky/caddy/etc/caddy
-docker --host ssh://scarif compose --project-name scarif up --detach --pull always --remove-orphans
+docker --host ssh://scarif compose --project-name scarif up --build --detach --pull always --remove-orphans

5311948877ce4584e04f8ad23373cff256b266e1
William Jackson <565174+williamjacksn@users.noreply.github.com>
2026-01-14 14:20:10 -0600

    Add skeleton deployment config

---
+++ ci/deploy.sh
@@ -0,0 +1,5 @@
+install --mode=600 -D /dev/null ~/.ssh/id_ed25519
+echo "${SSH_PRIVATE_KEY}" > ~/.ssh/id_ed25519
+cp ci/ssh_config ~/.ssh/config
+ssh-keyscan -H scarif.subtlecoolness.com > ~/.ssh/known_hosts
+docker --host scarif info
